4ca4510ba4086cef3dbdc4e75b835479550126a3
Nurul <njagirdar@virginmedia.com>
2023-11-07 01:39:29 +0000

    basic command parser functionality written

---
+++ ASE_Assignment/CommandParser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASE_Assignment
+{
+    internal class CommandParser
+    {
+        public CommandParser(string command, Canvass canvas, Pen pen)
+        {
+
+            string[] commandParts = command.Split(' ');
+
+            if (commandParts[0] == "circle" && int.TryParse(commandParts[1], out int radius))
+            {
+                Shape circle = new Circle(pen.Color, 10, 10, radius);
+                canvas.DrawShape(circle);
+            }
+
+        }
+    }
+}
