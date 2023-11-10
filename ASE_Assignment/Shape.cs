356cb0ba258897657f360bfa696347624b21cb08
Nurul <njagirdar@virginmedia.com>
2023-11-10 03:23:20 +0000

    XML documentation added for Shape class

--- ASE_Assignment/Shape.cs
+++ ASE_Assignment/Shape.cs
@@ -7,11 +7,20 @@ using System.Threading.Tasks;
 
 namespace ASE_Assignment
 {
+    /// <summary>
+    /// Represents an abstract shape with common properties - color and x,y position.
+    /// </summary>
     public abstract class Shape
     {
         protected Color colour;
         protected int x, y;
 
+        /// <summary>
+        /// Initialises new instance of Shape class with desired colour and x,y position.
+        /// </summary>
+        /// <param name="colour">Color of the shape.</param>
+        /// <param name="x">X position of the shape.</param>
+        /// <param name="y">Y position of the shape.</param>
         public Shape(Color colour, int x, int y)
         {
             this.colour = colour;
@@ -19,7 +28,15 @@ namespace ASE_Assignment
             this.y = y;
         }
 
+        /// <summary>
+        /// Draws the shape on the canvas using specified pen and fill state.
+        /// </summary>
+        /// <param name="g">GDI+ Grpahics object.</param>
+        /// <param name="point">Position to draw the shape.</param>
+        /// <param name="pen">Pen for drawing the shape.</param>
+        /// <param name="fill">Boolean determining to fill the shape or not.</param>
         public abstract void draw(Graphics g, Point point, Pen pen, bool fill);
+
 
         public override string ToString()
         {
