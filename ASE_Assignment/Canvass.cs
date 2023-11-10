ab98de58c5f8ad33e294df9c83a91fcc499cd06a
Nurul <njagirdar@virginmedia.com>
2023-11-10 03:13:16 +0000

    XML documentation added for Canvass

--- ASE_Assignment/Canvass.cs
+++ ASE_Assignment/Canvass.cs
@@ -7,15 +7,24 @@ using System.Threading.Tasks;
 
 namespace ASE_Assignment
 {
+    /// <summary>
+    /// Represents a canvas for drawing onto. It graphically represents the commands the user enters.
+    /// </summary>
     public class Canvass
     {
 
-        private Bitmap myBitmap;
-        private Graphics g;
-        private Point p;
-        private Pen pen;
-        private List<Shape> drawnShapes;
+        private Bitmap myBitmap; // Bitmap represents the canvas
+        private Graphics g; // GDI+ Graphics
+        private Point p; // The current drawing position/point
+        private Pen pen; // Pen used for drawing.
+        private List<Shape> drawnShapes; // List of shapes currently drawn on the canvas
 
+        /// <summary>
+        /// Initialises new instance of Canvass class.
+        /// </summary>
+        /// <param name="pen">Pen used for drawing.</param>
+        /// <param name="width">Width of the canvas.</param>
+        /// <param name="height">Height of the canvas.</param>
         public Canvass(Pen pen, int width, int height)
         {
             myBitmap = new Bitmap(width, height);
@@ -25,54 +34,93 @@ namespace ASE_Assignment
 
         }
 
+        /// <summary>
+        /// Draws a shape on the canvas using the draw method on the shape class. 
+        /// </summary>
+        /// <param name="shape">The shape to be drawn.</param>
+        /// <param name="fill">Boolean that determines to use fill or not</param>
         public void DrawShape(Shape shape, bool fill)
         {
             shape.draw(g, p, pen, fill);
             drawnShapes.Add(shape);
-
         }
 
+        /// <summary>
+        /// Gets a list of shapes currently drawn on the canvas.
+        /// </summary>
+        /// <returns>A list of shapes.</returns>
         public List<Shape> GetShapes()
         {
             return drawnShapes;
         }
 
+        /// <summary>
+        /// Clears the list of drawn shapes. Executed within the clear command in CommandParser.
+        /// </summary>
         public void ClearShapeList()
         {
             drawnShapes.Clear();
         }
 
+        /// <summary>
+        /// Gets the bitmap that represents the canvas.
+        /// </summary>
+        /// <returns>The bitmap representing the canvas.</returns>
         public Bitmap GetBitmap()
         {
             return myBitmap;
         }
 
-
+        /// <summary>
+        /// Moves the drawing position/point to coordinates entered.
+        /// </summary>
+        /// <param name="x">X position to move to.</param>
+        /// <param name="y">Y position to move to</param>
         public void MoveTo(int x, int y)
         {
             p = new Point(x, y);
         }
 
+        /// <summary>
+        /// Gets the current drawing position on the canvas.
+        /// </summary>
+        /// <returns>Current point.</returns>
         public Point GetPoint()
         {
             return p;
         }
 
+        /// <summary>
+        /// Gets the current pen.
+        /// </summary>
+        /// <returns>The current pen.</returns>
         public Pen GetPen()
         {
             return pen;
         }
 
+        /// <summary>
+        /// Draws a line from current position to the specified coordinates entered.
+        /// </summary>
+        /// <param name="pen">The pen used for drawing.</param>
+        /// <param name="x">X position of the lines end point</param>
+        /// <param name="y">Y position of the lines end point</param>
         public void DrawLine(Pen pen, int x, int y)
         {
             g.DrawLine(pen, p.X, p.Y, x, y);
         }
 
+        /// <summary>
+        /// Clears the canvas with a DarkGray colour.
+        /// </summary>
         public void Clear()
         {
             g.Clear(Color.DarkGray);   
         }
 
+        /// <summary>
+        /// Resets the Point position to its initial state - 0,0.
+        /// </summary>
         public void Reset()
         {
             p.X = 0; 
