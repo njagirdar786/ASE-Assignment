ff0c2148f28da4bd25a2f28b68b797d5a1ac2ee5
Nurul <njagirdar@virginmedia.com>
2023-11-10 03:45:30 +0000

    added XML documentation for GPLExceptions

--- ASE_Assignment/GPLexceptions.cs
+++ ASE_Assignment/GPLexceptions.cs
@@ -6,13 +6,24 @@ using System.Threading.Tasks;
 
 namespace ASE_Assignment
 {
+ 
+    /// <summary>
+    /// Class that holds custom exceptions related to this program (Graphical Programming Language).
+    /// </summary>
     public class GPLexceptions
     {
+        /// <summary>
+        /// Exception that is thrown when a command is invalid.
+        /// </summary>
         public class InvalidCommandException : Exception
         {
             public InvalidCommandException() { }
             public InvalidCommandException(string message) : base(message) { }
         }
+
+        /// <summary>
+        /// Exception that is thrown when invalid parameters are read.
+        /// </summary>
         public class InvalidParameterException : Exception 
         {
             public InvalidParameterException() { }
