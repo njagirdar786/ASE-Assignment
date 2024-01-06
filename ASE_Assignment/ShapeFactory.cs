a02223e0a89cebcad30cd3b4a8491d3e7d5f6585
Nurul <njagirdar@virginmedia.com>
2024-01-06 22:29:58 +0000

    XML docs for ShapeFactory

--- ASE_Assignment/ShapeFactory.cs
+++ ASE_Assignment/ShapeFactory.cs
@@ -7,8 +7,21 @@ using System.Threading.Tasks;
 
 namespace ASE_Assignment
 {
+    /// <summary>
+    /// Implements factory design pattern for shape creation.
+    /// </summary>
     public class ShapeFactory
     {
+        /// <summary>
+        /// Creates an instance of a shape based on given parameters.
+        /// </summary>
+        /// <param name="shapeType">Name of the shape.</param>
+        /// <param name="color">Color of the shape.</param>
+        /// <param name="x">X position of the shape.</param>
+        /// <param name="y">Y position of the shape.</param>
+        /// <param name="parameters">Width/Height for rectangle/triangle - Radius for circles.</param>
+        /// <returns>A new instance of a shape.</returns>
+        /// <exception cref="Exception">Exeception when shapeType is not recognised.</exception>
         public Shape CreateShape(string shapeType, Color color, int x, int y, params int[] parameters)
         {
             if (shapeType.ToLower() == "circle" && parameters.Length == 1)
